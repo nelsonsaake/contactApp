1e36caaab54c91030d4fba230ba7ba7a17568dee
unknown <nelsonsaakekofi@gmail.com>
2020-09-07 11:01:05 +0000

    login and register

---
+++ app/src/main/java/com/ampersand/contactapp/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.ampersand.contactapp.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
